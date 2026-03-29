5aeb71c2ae89a2f38cb1a0b1b142520efca28c90
thiaothiao <mamadou.thiao@gmail.com>
2026-03-29 21:09:23 +0200

    add softwares folder

---
+++ softwares/cpp/AtelierMainWindow.cpp
@@ -0,0 +1,13 @@
+#include "AtelierMainWindow.h"
+
+#include "AtelierWidget.h"
+
+AtelierMainWindow::AtelierMainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    this->setWindowTitle("CUSTOM SOFTWARE");
+    this->setMinimumSize(640, 500);
+    auto* main_widget = new AtelierWidget(this);
+    this->setCentralWidget(main_widget);
+
+}
---
+++ softwares/cpp/AtelierMainWindow.h
@@ -0,0 +1,14 @@
+#ifndef SPARSEPC_ATELIER_MAINWINDOW_HPP
+#define SPARSEPC_ATELIER_MAINWINDOW_HPP
+
+#include <QtWidgets/QMainWindow>
+
+class AtelierMainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    AtelierMainWindow(QWidget *parent = Q_NULLPTR);
+};
+
+#endif //SPARSEPC_ATELIER_MAINWINDOW_HPP
