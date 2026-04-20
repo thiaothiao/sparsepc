6864a085c2df3e5823e3485ece67492c0e6bcac4
thiaothiao <mamadou.thiao@gmail.com>
2026-04-20 17:25:14 +0200

    pcs radio button and types layout form

--- sparsely/labwidget.h
+++ sparsely/labwidget.h
@@ -16,6 +16,7 @@ class QSlider;
 class QStackedLayout;
 class QComboBox;
 class QPushButton;
+class QRadioButton;
 
 class JKQTPPlotElement;
 class JKQTPlotter;
@@ -79,6 +80,8 @@ namespace sparsely
         QComboBox *m_PlotTypeComboBox;
         QPushButton *m_AddNewSparseComponentButton;
         QPushButton *m_RemoveLastSparseComponentButton;
+        QRadioButton *m_SparsePCRadioButton;
+        QRadioButton *m_StandardPCRadioButton;
         QWidget *m_ButtonsWidget;
         QVector<QString> m_Colors;
 
