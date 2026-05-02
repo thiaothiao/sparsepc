dc54e5217f37f67c8d637bc364812ee44c296308
thiaothiao <mamadou.thiao@gmail.com>
2026-05-02 17:39:43 +0200

    add qt logger

--- sparsely/main.cpp
+++ sparsely/main.cpp
@@ -1,17 +1,60 @@
 #include <QApplication>
 #include <QDir>
+#include <QFile>
 #include <QIcon>
 #include <QPixmap>
 #include <QSplashScreen>
+#include <QTextStream>
+#include <QtLogging>
+
 #include <labmainwindow.h>
 
 #include <thread>
 
 #include <infos.h>
 
+namespace
+{
+    void messageHandler(QtMsgType type, const QMessageLogContext &context,
+                        const QString &message)
+    {
+        QFile logFile("sparsely.log");
+        if (logFile.open(QIODevice::WriteOnly | QIODevice::Append |
+                         QIODevice::Text))
+        {
+            QTextStream out(&logFile);
+            out << QDateTime::currentDateTime().toString(
+                "yyyy-MM-dd hh:mm:ss ");
+            switch (type)
+            {
+            case QtDebugMsg:
+                out << "DEBUG: ";
+                break;
+            case QtWarningMsg:
+                out << "WARNING: ";
+                break;
+            case QtCriticalMsg:
+                out << "CRITICAL: ";
+                break;
+            case QtFatalMsg:
+                out << "FATAL: ";
+                break;
+            case QtInfoMsg:
+                out << "INFO: ";
+                break;
+            }
+            out << message << " (" << context.file << ":" << context.line << ")"
+                << Qt::endl;
+        }
+    }
+} // namespace
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    qInstallMessageHandler(messageHandler);
+    qInfo() << "Sparsely Start";
+
     QApplication::setStyle("fusion"); // force fusion style
     app.setApplicationName(
         QString::fromStdString(std::string(sparsely::metadata::appName)));
